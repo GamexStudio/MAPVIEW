fdf91af05ed4ff0ff50f57d4be1a74e587e19f14
Ashish Chauhan <ashishchauhan@ashishs-macbook-pro.local>
2015-12-07 10:29:04 +0530

    Initial Commit

---
+++ GetLatLong/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GetLatLong
+//
+//  Created by Ashish Chauhan on 07/12/15.
+//  Copyright © 2015 Ashish Chauhan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ GetLatLong/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GetLatLong
+//
+//  Created by Ashish Chauhan on 07/12/15.
+//  Copyright © 2015 Ashish Chauhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
